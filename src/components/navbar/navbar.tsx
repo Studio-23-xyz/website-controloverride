28a1ca4ef935022954bd00dc5dc34701f886e07e
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-07-11 20:29:08 +0600

    navbar enhanced

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import classes from './navbar.module.css';
 
-
 type NavButtonProps = {
   href: string;
   label: string;
@@ -22,14 +21,20 @@ const NavButton: React.FC<NavButtonProps> = ({ href, label, targetBlank = false,
 );
 
 const NavBar: React.FC = () => (
-  <div className="flex justify-between items-center bg-gray-800 py-4 px-8">
-    <NavButton href="https://studio-23.xyz/" label="HOME" targetBlank />
-    <NavButton href="#features" label="FEATURES" />
-    <NavButton href="#screenshots" label="SCREENSHOTS" />
-    <NavButton href="https://drive.google.com/drive/folders/1cInz9pylfEsPm-2y3p5G-49LSlsiVw44?fbclid=IwAR0v7tcjbP1m_3iQ28pPmFCh42VNuKjlOLlIOWQ3L-xipwj19-qnXvo8s58" label="PRESS KIT" targetBlank />
-    <NavButton href="https://studio-23.xyz/contact/" label="CONTACT US" targetBlank />
-    <NavButton href="https://store.steampowered.com/app/1406090/ControlOverride/" label="BUY NOW" special targetBlank />
-  </div>
+  <nav className="bg-gray-800 py-4 px-4 md:px-8">
+    <div className="flex flex-col md:flex-row md:justify-between items-center space-y-2 md:space-y-0">
+      <div className="flex flex-col md:flex-row md:space-x-4 space-y-2 md:space-y-0">
+        <NavButton href="https://studio-23.xyz/" label="HOME" targetBlank />
+        <NavButton href="#features" label="FEATURES" />
+        <NavButton href="#screenshots" label="SCREENSHOTS" />
+        <NavButton href="https://drive.google.com/drive/folders/1cInz9pylfEsPm-2y3p5G-49LSlsiVw44?fbclid=IwAR0v7tcjbP1m_3iQ28pPmFCh42VNuKjlOLlIOWQ3L-xipwj19-qnXvo8s58" label="PRESS KIT" targetBlank />
+        <NavButton href="https://studio-23.xyz/contact/" label="CONTACT US" targetBlank />
+      </div>
+      <div>
+        <NavButton href="https://store.steampowered.com/app/1406090/ControlOverride/" label="BUY NOW" special targetBlank />
+      </div>
+    </div>
+  </nav>
 );
 
 export default NavBar;
